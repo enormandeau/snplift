af8137606604e9933753a0009d36a093231525c2
Eric Normandeau <eric.normandeau.qc@gmail.com>
2022-05-19 13:08:35 -0400

    Update TODO list

--- runall.sh
+++ runall.sh
@@ -12,8 +12,9 @@ WINDOW_LENGTH=100 # If you modify this value, also modify 06_score_markers.py
 NUM_NEIGHBOURS=10
 
 # TODO Rename chromosomes to remove anything after a space
-# TODO Validate that all scaffolds have unique names in each genomes
-# TODO Check nucleotide distance of first chromosome
+#      - Names of the old genome must be the same as those in the VCF
+#      - Validate that all scaffolds have unique names in each genomes
+#      Check nucleotide distance of genomes using first chromosome
 
 # Check collinearity
 echo "SNPTransfer: Assessing collinearity of the two genomes"
