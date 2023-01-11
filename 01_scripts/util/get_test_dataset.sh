6eaeb6c5f04719247f60836b11abeaae92eaef42
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-01-11 14:43:05 -0500

    Report test data prep steps

--- 01_scripts/util/get_test_dataset.sh
+++ 01_scripts/util/get_test_dataset.sh
@@ -2,7 +2,14 @@
 # Get test dataset from github and prepare it for a snplift run
 
 rm -rf snplift_test_dataset 2>/dev/null
+
+echo -e "\nSNPlift: Cloning test data\n"
 git clone git@github.com:enormandeau/snplift_test_dataset
+
+echo -e "\nSNPlift: Extracting test data\n"
 gunzip -c snplift_test_dataset/new_genome.fasta.gz > 03_genomes/new_genome.fasta
 gunzip -c snplift_test_dataset/old_genome.fasta.gz > 03_genomes/old_genome.fasta
 gunzip -c snplift_test_dataset/old.vcf.gz > 04_input_vcf/old.vcf
+
+
+echo -e "\nSNPlift: Ready to launch analysis with:\n\n    ./snplift 02_infos/snplift_config.sh\n"
