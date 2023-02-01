bd2db4a1a4fa305fbe478f4249c98709f34886ff
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-02-01 15:38:30 -0500

    Default NCPUS -> 10

--- 02_infos/snplift_config.sh
+++ 02_infos/snplift_config.sh
@@ -19,7 +19,7 @@ export CHECK_COLLINEARITY=0 # Increases runtime by ~5 times. Align genomes and p
                             #  collinearity comparison figure [0, 1]
 
 # Parameters
-export NCPUS=20             # Number of cores to use for mapping steps (miniconda2, bwa mem)
+export NCPUS=10             # Number of cores to use (around 10 and maximum 20 is recommended)
 export WINDOW_LENGTH=100    # If you modify this value, also modify 06_score_markers.py
 export NUM_NEIGHBOURS=10    # Number of neighbour SNPs to consider when trying to recuperate
                             #   more dubious SNPs using local correlations of positions
