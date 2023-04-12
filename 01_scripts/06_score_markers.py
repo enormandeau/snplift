b9bb86fe26068530010e3bb12f09d973e2e0ad50
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-04-12 13:44:56 -0400

    New filters and default values

--- 01_scripts/06_score_markers.py
+++ 01_scripts/06_score_markers.py
@@ -26,7 +26,6 @@ with open(input_features, "rt") as infile:
             l = line.strip().split("\t")
 
             if line.startswith("QueryName"):
-                #header = l[:]
                 header =  ["QueryScaffold", "QueryName", "QueryPos", "TargetChrom", "TargetPos"]
                 header.insert(0, "Penalties")
                 header.insert(0, "Score")
@@ -41,7 +40,7 @@ with open(input_features, "rt") as infile:
             query_scaffold = QueryName.split(";")[0]
 
             # Alignment too short
-            if len(Sequence) < (expected_length / 2):
+            if len(Sequence) < (expected_length / 1.5):
                 penalties.append("L")
                 score -= 1.0
 
@@ -55,9 +54,9 @@ with open(input_features, "rt") as infile:
                 penalties.append("Q")
 
                 if int(MappingQuality) < 5:
-                    score -= 1.0
+                    score -= 0.8
                 else:
-                    score -= 0.6
+                    score -= 0.4
 
             # Number of differences over best supplementary alignment
             if int(SuppAlignMinDiff) <= 5:
@@ -66,25 +65,23 @@ with open(input_features, "rt") as infile:
 
             # More than 5% difference to reference genome
             if int(NumDiff) > 0.05 * len(Sequence):
-                penalties.append("D")
-                score -= 0.3
+                penalties.append("d")
+                score -= 0.2
 
-            # TODO Redundancy in treating Softclip?
-            # Maximum 25% softclip
+                # More than 10% difference to reference genome
+                if int(NumDiff) > 0.10 * len(Sequence):
+                    penalties.append("D")
+                    score -= 0.2
+
+            # Maximum 10% softclip that are not Ns
             if (int(Softclip) - int(NumNs)) > 0.10 * len(Sequence):
                 penalties.append("s")
                 score -= 0.2
 
-            # Match plus softclip are not at least 90% of sequence
+            # Match plus softclip and Ns are not at least 90% of sequence
             if (int(Match) + int(Softclip) + int(NumNs)) < 0.9 * len(Sequence):
                 penalties.append("P")
                 score -= 0.3
-
-            # (Number of softclip minus 5% of sequence length) / (number of differences + 1) > 1.1
-            if ((int(Softclip) - 0.05 * len(Sequence)) / (int(NumDiff) + 1)) > 1.1:
-                penalties.append("S")
-                score -= 0.5
-
 
             if not penalties:
                 penalties.append(".")
--- 02_infos/snplift_config.sh
+++ 02_infos/snplift_config.sh
@@ -24,6 +24,6 @@ export NCPUS=10             # Number of cores to use (around 10 and maximum 20 i
                             #   For less than 100K SNPs, 1 to 4 cores is a good choice
                             #   For less than 1M SNPs, 10 cores is a good choice
                             #   Above this, 20 cores is going to be slightly faster
-export WINDOW_LENGTH=100    # If you modify this value, also modify 06_score_markers.py
-export NUM_NEIGHBOURS=10    # Number of neighbour SNPs to consider when trying to recuperate
+export WINDOW_LENGTH=200    # Sequence size kept on both sides of each SNP
+export NUM_NEIGHBOURS=20    # Number of neighbour SNPs to consider when trying to salvage
                             #   more dubious SNPs using local correlations of positions
