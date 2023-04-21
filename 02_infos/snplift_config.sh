0bc2bf6ef689ee069649f712c5abcc1ad1c8bafd
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-04-21 09:05:44 -0400

    Document config file

--- 02_infos/snplift_config.sh
+++ 02_infos/snplift_config.sh
@@ -19,22 +19,24 @@ export SKIP_VISUALIZATION=1 # Avoid creating a plot to explore features. These a
 export CHECK_COLLINEARITY=0 # Increases runtime by 5+ times. Align genomes and produce a
                             #   collinearity comparison figure [0, 1].
 
-# Parameters
+# Number of CPUs
 export NCPUS=10             # Number of cores to use (around 10 and maximum 20 is recommended)
                             #   For less than 100K SNPs, 1 to 4 cores is a good choice
                             #   For less than 1M SNPs, 10 cores is a good choice
                             #   Above this, 20 cores is going to be slightly faster
 
+# Size of flanking regions
 export WINDOW_LENGTH=300    # Sequence size kept on both sides of each SNP. For highly contiguous
                             #   genomes, 300 leads to more positions being tranfered. For more
                             #   fragmented genomes, using 200 or even 100 may produce better results.
 
+# Number of neighbours to use
 export NUM_NEIGHBOURS=20    # Number of neighbour SNPs to consider when trying to salvage
                             #   more dubious SNPs using local correlations of positions
 
 # Do final corrections to VCF file. You may need
-export UNIQUE_POS=1         # Keep only the first locus encountered for each position
-export CORRECT_ID=1         # Recompute the ID column from columns 1 and 2
+export UNIQUE_POS=1         # Keep only the first locus encountered for each position [0, 1]
+export CORRECT_ID=1         # Recompute the ID column from columns 1 and 2 [0, 1]
 export ID_COLUMN=3          # ID column number
-export CORRECT_ALLELES=1    # Reverse complement alleles when new genome region is inversed
+export CORRECT_ALLELES=1    # Reverse complement alleles when new genome region is inversed [0, 1]
 export ALLELE_COLUMNS=4,5   # Columns of alleles to reverse complement
