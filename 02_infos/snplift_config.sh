6e667f221875f0dd35a92baf921108fb3510eb24
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-04-26 15:28:18 -0400

    Minor: Doc in config file

--- 02_infos/snplift_config.sh
+++ 02_infos/snplift_config.sh
@@ -9,24 +9,25 @@ export INPUT_FILE="04_input_vcf/old.vcf"
 export OUTPUT_FILE="new.vcf"
 
 # Skipping genome indexing
-export SKIP_INDEXING=1      # Save time if genome already indexed with 'bwa index' [0, 1].
-
-# Skip exploring features
-export SKIP_VISUALIZATION=1 # Avoid creating a plot to explore features. These are used
-                            #   for debugging [0, 1].
+export SKIP_INDEXING=1      # Use 0 if genome is not indexed. Use 1 to save time if genome already
+                            # indexed with 'bwa index' [0, 1].
 
 # Checking for collinearity between both genome versions
 export CHECK_COLLINEARITY=0 # Increases runtime by 5+ times. Align genomes and produce a
                             #   collinearity comparison figure [0, 1].
 
+# Skip exploring features
+export SKIP_VISUALIZATION=1 # Avoid creating a plot to explore features. These are used
+                            #   for debugging [0, 1].
+
 # Number of CPUs
 export NCPUS=10             # Number of cores to use (around 10 and maximum 20 is recommended)
-                            #   For less than 100K SNPs, 1 to 4 cores is a good choice
-                            #   For less than 1M SNPs, 10 cores is a good choice
-                            #   Above this, 20 cores is going to be slightly faster
+                            #   For less than 100K SNPs, 1 to 4 cores is a good choice.
+                            #   For less than 1M SNPs, 10 cores is a good choice.
+                            #   Above this, 20 cores is going to be slightly faster.
 
 # Size of flanking regions
-export WINDOW_LENGTH=300    # Sequence size kept on both sides of each SNP. For highly contiguous
+export WINDOW_LENGTH=300    # Sise of sequences kept on both sides of each SNP. For highly contiguous
                             #   genomes, 300 leads to more positions being tranfered. For more
                             #   fragmented genomes, using 200 or even 100 may produce better results.
 
@@ -34,9 +35,9 @@ export WINDOW_LENGTH=300    # Sequence size kept on both sides of each SNP. For 
 export NUM_NEIGHBOURS=20    # Number of neighbour SNPs to consider when trying to salvage
                             #   more dubious SNPs using local correlations of positions
 
-# Do final corrections to VCF file. You may need
-export UNIQUE_POS=1         # Keep only the first locus encountered for each position [0, 1]
-export CORRECT_ID=1         # Recompute the ID column from columns 1 and 2 [0, 1]
-export ID_COLUMN=3          # ID column number
-export CORRECT_ALLELES=1    # Reverse complement alleles when new genome region is inversed [0, 1]
-export ALLELE_COLUMNS=4,5   # Columns of alleles to reverse complement
+# Do final corrections to VCF file
+export UNIQUE_POS=1         # Keep only the first locus encountered for each position [0, 1].
+export CORRECT_ID=1         # Recompute the ID column from columns 1 and 2 [0, 1].
+export ID_COLUMN=3          # ID column number [int]. Only used for `CORRECT_ID=1`.
+export CORRECT_ALLELES=1    # Reverse complement alleles when new genome region is inversed [0, 1].
+export ALLELE_COLUMNS=4,5   # Columns of alleles to reverse complement. Only used for `CORRECT_ALLELES=1`
