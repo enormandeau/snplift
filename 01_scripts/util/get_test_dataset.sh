8dd9ea0d171c84ae34aeeb969bd65e3e6770ac85
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-06-20 14:14:45 -0400

    Update git clone link to html

--- 01_scripts/util/get_test_dataset.sh
+++ 01_scripts/util/get_test_dataset.sh
@@ -4,7 +4,7 @@
 rm -rf snplift_test_dataset 2>/dev/null
 
 echo -e "\nSNPlift: Cloning test data\n"
-git clone git@github.com:enormandeau/snplift_test_dataset
+git clone https://github.com/enormandeau/snplift_test_dataset
 
 echo -e "\nSNPlift: Extracting test data"
 gunzip -c snplift_test_dataset/new_genome.fasta.gz > 03_genomes/new_genome.fasta
