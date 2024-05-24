bab23e6851537ac14b4c7899693e3fcf7d55660a
Eric Normandeau <eric.normandeau.qc@gmail.com>
2024-05-24 07:57:22 -0400

    Update default parameters

--- 02_infos/snplift_config.sh
+++ 02_infos/snplift_config.sh
@@ -5,8 +5,8 @@ export OLD_GENOME="03_genomes/old_genome.fasta"
 export NEW_GENOME="03_genomes/new_genome.fasta"
 
 # Output files
-export INPUT_FILE="04_input_vcf/old.vcf"
-export OUTPUT_FILE="new.vcf"
+export INPUT_FILE="04_input_vcf/outliers_pcadapt_Chr13_window.txt"
+export OUTPUT_FILE="outliers_pcadapt_Chr13_window_transfered_positions.txt"
 
 # Skipping genome indexing
 export SKIP_INDEXING=1      # Use 0 if genome is not indexed. Use 1 to save time if genome already
@@ -21,7 +21,7 @@ export SKIP_VISUALIZATION=1 # Avoid creating a plot to explore features. These a
                             #   for debugging [0, 1].
 
 # Number of CPUs
-export NCPUS=20             # Number of cores to use (around 10 and maximum 20 is recommended)
+export NCPUS=1             # Number of cores to use (around 10 and maximum 20 is recommended)
                             #   For less than 100K SNPs, 1 to 4 cores is a good choice.
                             #   For less than 1M SNPs, 10 cores is a good choice.
                             #   Above this, 20 cores is going to be slightly faster.
@@ -36,13 +36,13 @@ export NUM_NEIGHBOURS=20    # Number of neighbour SNPs to consider when trying t
                             #   more dubious SNPs using local correlations of positions.
 
 # Do final corrections to VCF file
-export CORRECT_ID=1         # Recompute the ID column from columns 1 and 2 [0, 1].
+export CORRECT_ID=0         # Recompute the ID column from columns 1 and 2 [0, 1].
 export ID_COLUMN=3          # ID column number [int]. Only used for `CORRECT_ID=1`.
-export CORRECT_ALLELES=1    # Reverse complement alleles when new genome region is inverted [0, 1].
+export CORRECT_ALLELES=0    # Reverse complement alleles when new genome region is inverted [0, 1].
 export ALLELE_COLUMNS=4,5   # Columns of alleles to reverse complement. Only used for `CORRECT_ALLELES=1`.
 
 # Memory hungry options
 export UNIQUE_POS=1         # Keep only the first locus encountered for each position [0, 1]. Memory hungry.
-export SORT_OUTPUT=0        # Sort VCF or output file according to the two first columns.
+export SORT_OUTPUT=1        # Sort VCF or output file according to the two first columns.
                             #   WARNING: For big VCFs, this will require a lot of RAM, around 3X the size
                             #   of the output file.
