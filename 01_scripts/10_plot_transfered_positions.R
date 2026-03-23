477c05de16a70b68cece12578bd34d3db5794d02
Eric Normandeau <eric.normandeau@qc.gmail.com>
2026-03-23 12:55:55 -0400

    Add 10_plot_transfered_positions.R

---
+++ 01_scripts/10_plot_transfered_positions.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+# Parse user input
+rm(list=ls())
+args = commandArgs(trailingOnly=TRUE)
+input_file = args[1]
+output_file = paste0(input_file, ".png")
+
+# Load data
+data = read.table(input_file)
+
+# Create plot
+png(width=1150, height=1150)
+
+plot(data[,2] ~ data[,4],
+     pch=19,
+     col="darkred",
+     cex=0.4,
+     main=paste0("Corresponding positions of ", nrow(data), " transfered SNP"),
+     xlab="Position in genome 1",
+     ylab="Position in genome 2")
+
+dev.off()
